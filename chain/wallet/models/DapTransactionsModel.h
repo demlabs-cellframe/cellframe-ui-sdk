ae15fdbf8a280d3cc7569ad48a63406b3d08afef
Postnickov <postnickov.lyoha@yandex.ru>
2020-11-25 13:43:53 +0300

    add date role

--- chain/wallet/models/DapTransactionsModel.h
+++ chain/wallet/models/DapTransactionsModel.h
@@ -9,12 +9,17 @@ class DapTransactionsModel : public QAbstractListModel
     Q_OBJECT
 public:
 
+    enum Roles {
+        DateDisplayRole = Qt::UserRole
+    };
+
     explicit DapTransactionsModel(QObject *parent = nullptr);
 
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
 
     Q_INVOKABLE void append(DapTransaction* a_transaction);
+    QHash<int, QByteArray> roleNames() const override;
 
 private:
     QList<DapTransaction*> m_transactions;
